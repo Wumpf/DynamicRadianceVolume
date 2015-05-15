a411e3f81f8130a1c55abcdedf237e522d81a57e
Andreas Reich <r_andreas2@web.de>
2015-05-15 10:44:44 +0200

    Decomposed BRDF function in several helper functions.

--- ivct/shader/lightingfunctions.glsl
+++ ivct/shader/lightingfunctions.glsl
@@ -3,10 +3,10 @@
 float ComputeSpotFalloff(float cosToLight)
 {
 	// Linear falloff as used in the Mitsuba renderer
-	return saturate(acos(LightCosHalfAngle) - acos(cosToLight)) / (acos(LightCosHalfAngle));
+	//return saturate(acos(LightCosHalfAngle) - acos(cosToLight)) / (acos(LightCosHalfAngle));
 
 	// Much nicer and faster falloff
-	//return saturate(cosToLight - LightCosHalfAngle) / (1.0 - LightCosHalfAngle);
+	return saturate(cosToLight - LightCosHalfAngle) / (1.0 - LightCosHalfAngle);
 }
 
 float ComputeSpotFalloff(vec3 toLight)
@@ -15,23 +15,38 @@ float ComputeSpotFalloff(vec3 toLight)
 }
 
 
+// Converting the parameters Roughness and Metalness to BRDF specific values is not entirely straigh forward.
+// Some useful resources on the topic:
+// - Feeding a physically based shading model https://seblagarde.wordpress.com/2011/08/17/feeding-a-physical-based-lighting-mode/
+// - Marmoset PBR Texture Conversion Tutorial: http://www.marmoset.co/toolbag/learn/pbr-conversion
+// - http://www.marmoset.co/toolbag/learn/pbr-practice
+// - Unity Material charts http://blogs.unity3d.com/2015/02/18/working-with-physically-based-shading-a-practical-approach/
+// 		(this gives a good hint of what they do internally with metallic) 
+
+float RoughnessToBlinnExponent(float roughness)
+{
+	return 1.0 / (roughness * roughness + 0.0005); // Completely made up
+}
+float BlinnNormalization(float blinnExponent)
+{
+	return (blinnExponent + 8.0) / 8.0;
+}
+void ComputeMaterialColors(vec3 baseColor, float metallic, out vec3 diffuseColor, out vec3 specularColor)
+{
+	diffuseColor = mix(baseColor, vec3(0.02), metallic);	// Almost dark for Metalic values
+	specularColor = mix(vec3(0.0356), baseColor, metallic); // Greyish for specular. Matches value of Marmoset Tutorial which fits into the range given at the unity material chart.
+}
+
 // Evaluates default BRDF.
 // Attention: Mind vector directions of incoming parameters!
 vec3 BRDF(vec3 toLight, vec3 toCamera, vec3 normal, vec3 baseColor, vec2 roughnessMetalic)
 {
 	// Normalized Blinn Phong as in Realtime Rendering (3rd edition) page 257 (eq 7.48)
+	float blinnExponent = RoughnessToBlinnExponent(roughnessMetalic.x);
+	vec3 diffuseColor, specularColor;
+	ComputeMaterialColors(baseColor, roughnessMetalic.y, diffuseColor, specularColor);
 
-	// Converting the parameters Roughness and Metalness to BRDF specific values is not entirely straigh forward.
-	// Some useful resources on the topic:
-	// - Feeding a physically based shading model https://seblagarde.wordpress.com/2011/08/17/feeding-a-physical-based-lighting-mode/
-	// - Marmoset PBR Texture Conversion Tutorial: http://www.marmoset.co/toolbag/learn/pbr-conversion
-	// - Unity Material charts http://blogs.unity3d.com/2015/02/18/working-with-physically-based-shading-a-practical-approach/
-	// 		(this gives a good hint of what they do internally with metallic) 
-	float blinnExponent = 1.0 / (roughnessMetalic.x * roughnessMetalic.x + 0.0005); // Completely made up
-	vec3 diffuseColor = mix(baseColor, vec3(0.02), roughnessMetalic.y);	// Almost dark for Metalic values
-	vec3 specularColor = mix(vec3(0.0356), baseColor, roughnessMetalic.y); // Greyish for specular. Matches value of Marmoset Tutorial which fits into the range given at the unity material chart.
-
-	float normalization = (blinnExponent + 8.0) / 8.0;
+	float normalization = BlinnNormalization(blinnExponent);
 	vec3 halfVector = normalize(toCamera + toLight);
 	float blinnPhong = normalization * pow(saturate(dot(normal, halfVector)), blinnExponent);
 
