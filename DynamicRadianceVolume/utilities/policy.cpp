fda66e67bf0f658b49d708bd7dd7c4150688da01
Andreas Reich <r_andreas2@web.de>
2015-09-19 13:19:42 +0200

    Logging function now with cout instead of exception.

--- DynamicRadianceVolume/utilities/policy.cpp
+++ DynamicRadianceVolume/utilities/policy.cpp
@@ -8,13 +8,14 @@ namespace Logger {
 
 	FilePolicy::FilePolicy(const std::string& _fileName) : m_file(_fileName)
 	{
-		if( !m_file )
-			throw std::runtime_error("Cannot open file '" + _fileName + "' for logging.");
+		if (!m_file)
+			std::cout << "Cannot open file '" << _fileName.c_str() << "' for logging.";//throw std::runtime_error("Cannot open file '" + _fileName + "' for logging.");
 	}
 	
 	void FilePolicy::Write( const std::string& _message )
 	{
-		m_file << _message;
+		if (m_file)
+			m_file << _message;
 		std::cout << _message;
 	}
 
